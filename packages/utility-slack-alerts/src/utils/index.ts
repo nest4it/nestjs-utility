19ab494fee130ea91f45a8f9003391769e3648d3
Robert-Jan Kuyper <info@datails.nl>
2025-01-10 23:23:22 +0100

    fix: slack alerts

--- packages/utility-slack-alerts/src/utils/index.ts
+++ packages/utility-slack-alerts/src/utils/index.ts
@@ -1,17 +1,33 @@
 import { Inject } from '@nestjs/common';
 import { SLACK_CLIENT } from '../constants';
-import { Message, Section } from 'slack-block-builder';
 import type { ErrorObj } from '../models';
 
-export const createAlert = (emoji: string, severity: string) => (obj: ErrorObj) =>
-  Message()
-    .asUser()
-    .blocks(
-      Section({ text: `:${emoji}: *${obj.message}* :${emoji}:` }),
-      Section({ text: `*Severity*: ${severity}` }),
-      Section({ text: `*Time*: ${new Date().toDateString()}` }),
-    )
-    .buildToJSON();
+export const createAlert = (emoji: string, severity: string) => (obj: ErrorObj) => ({
+  as_user: true,
+  blocks: [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `:${emoji}: *${obj.message}* :${emoji}:`,
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*Severity*: ${severity}`,
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*Time*: ${new Date().toDateString()}`,
+      },
+    },
+  ],
+});
 
 export const createFailureAlert = createAlert('warning', 'Failure');
 export const createErrorAlert = createAlert('red_circle', 'Error');
