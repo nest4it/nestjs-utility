00bc6d41cbd0e9d136bf99191ca5a40cb9e11acd
Simon van de Fliert <simonfliert@hotmail.com>
2025-01-16 20:14:31 +0100

    refactor: Removed unused code.

--- packages/utility-api-key/src/utils/decode.ts
+++ packages/utility-api-key/src/utils/decode.ts
@@ -3,23 +3,11 @@ import {
   validateAuthenticatedClient,
   type AuthenticatedClient,
 } from '../models/authenticated-client';
-import { ApiKeyExpiredError } from '../errors';
-
-export const toSeconds = (time: number) => Math.floor(time / 1000);
-
-export const getCurrentTimeInSeconds = () => toSeconds(Date.now());
-
-export const isJwtTokenExpired = (token: AuthenticatedClient) =>
-  token.exp < getCurrentTimeInSeconds();
 
 export const createVerifyJwtToken = (secret: string) => async (token: string) => {
   const payload = verify(token, secret, { ignoreExpiration: false });
 
   const parsedToken = validateAuthenticatedClient<AuthenticatedClient>(payload);
-
-  if (isJwtTokenExpired(parsedToken)) {
-    throw new ApiKeyExpiredError('Token has expired');
-  }
 
   return parsedToken;
 };
