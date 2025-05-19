57886614909a0597bc249b4274e37cba1a0a864d
Robert-Jan Kuyper <info@datails.nl>
2025-05-19 16:53:54 +0200

    fix: nest 11 and 10

--- packages/utility-api-key/src/strategies/api-key.strategy.ts
+++ packages/utility-api-key/src/strategies/api-key.strategy.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import Strategy from 'passport-headerapikey';
+import type { Function } from 'ts-toolbelt';
 import { API_KEY_MODULE_STRATEGY } from '../constants';
 import { ApiKeyService } from '../api-key.service';
 import type { ApiKeyModuleConfig } from '../models/config';
@@ -19,14 +20,31 @@ export class ApiKeyStrategy extends PassportStrategy(Strategy, API_KEY_MODULE_ST
         prefix: options.apiKeyHeaderPrefix ?? DEFAULTS.apiKeyHeaderPrefix,
       },
       true,
+      // @ts-expect-error we need to override the default verify function
+      async (apiKey: string, done: ValidateFn) => this.validate(apiKey, done),
     );
   }
 
-  public async validate(apiKey: string) {
-    return this.tokenService.verifyApiKey(apiKey).catch((err) => {
-      new UnauthorizedException({
-        cause: err,
+  public async validate(apiKey: string, done: Function.Function) {
+    // nest ^11 support
+    if (typeof done !== 'function') {
+      return this.tokenService.verifyApiKey(apiKey).catch((err) => {
+        throw new UnauthorizedException({
+          cause: err,
+        });
       });
-    });
+    }
+
+    return this.tokenService
+      .verifyApiKey(apiKey)
+      .then((user) => done(null, user))
+      .catch((err) => {
+        done(
+          new UnauthorizedException({
+            cause: err,
+          }),
+          null,
+        );
+      });
   }
 }
